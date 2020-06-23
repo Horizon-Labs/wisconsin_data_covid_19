253ab292f0b214f819e97641cb26ba07e8b1f874
Rose T <roset21@muhs.edu>
2020-06-23 12:44:29 -0500

    demo

--- build/lib/covid_data/census.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-def fetch_data():
-    full_census = pd.read_csv('https://www.census.gov/content/dam/Census/topics/research/pdb2020stcov2_us.csv', encoding = "ISO-8859-1")
-    wisconsin_census = full_census[full_census.State_name == 'Wisconsin']
-    wisconsin_census.head(1)
--- build/lib/covid_data/testData/testData.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import plotly.figure_factory as ff
-import random
-
